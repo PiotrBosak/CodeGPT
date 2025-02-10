817826e198a76d51f462055d8c7b1ffea66a24c1
Piotr Bosak <piotrbosak@Piotrs-MacBook-Pro.local>
2025-02-10 11:15:16 +0100

    next debug commit

--- src/main/java/ee/carlrobert/codegpt/toolwindow/chat/ToolWindowCompletionResponseEventListener.java
+++ src/main/java/ee/carlrobert/codegpt/toolwindow/chat/ToolWindowCompletionResponseEventListener.java
@@ -68,10 +68,28 @@ abstract class ToolWindowCompletionResponseEventListener implements
 
     public abstract void handleTokensExceededPolicyAccepted();
 
-  @Override
-  public void handleRequestOpen() {
-    updateTimer.start();
-  }
+    @Override
+    public void handleRequestOpen() {
+        try {
+            var homeDirectory = System.getProperty("user.home");
+            var file = new File(homeDirectory + "/code-gpt-output.md");
+            var documentManager = FileDocumentManager.getInstance();
+            var virtualFile = VfsUtil.findFileByIoFile(file, true);
+            ActionsKt.runReadAction(() -> {
+                assert virtualFile != null;
+                var document = documentManager.getDocument(virtualFile);
+                ActionsKt.runWriteAction(() -> {
+                    assert document != null;
+                    document.insertString(document.getTextLength(), "\n## AI\n");
+                    virtualFile.refresh(false, false);
+                    return Unit.INSTANCE;
+                });
+                return Unit.INSTANCE;
+            });
+        } catch (Exception e) {
+        }
+        updateTimer.start();
+    }
 
   @Override
   public void handleMessage(String partialMessage) {
@@ -128,6 +146,26 @@ abstract class ToolWindowCompletionResponseEventListener implements
   @Override
   public void handleCompleted(String fullMessage, ChatCompletionParameters callParameters) {
     conversationService.saveMessage(fullMessage, callParameters);
+
+    try {
+        var homeDirectory = System.getProperty("user.home");
+        var file = new File(homeDirectory + "/code-gpt-output.md");
+        var documentManager = FileDocumentManager.getInstance();
+        var virtualFile = VfsUtil.findFileByIoFile(file, true);
+        ActionsKt.runReadAction(() -> {
+            assert virtualFile != null;
+            var document = documentManager.getDocument(virtualFile);
+            ActionsKt.runWriteAction(() -> {
+                assert document != null;
+                document.insertString(document.getTextLength(), "\n## End AI\n");
+                virtualFile.refresh(false, false);
+                return Unit.INSTANCE;
+            });
+            return Unit.INSTANCE;
+        });
+    } catch (Exception e) {
+
+    }
 
     ApplicationManager.getApplication().invokeLater(() -> {
       try {
